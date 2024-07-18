5b36418d6b0ff387393458f26c23c613e85e5ec8
Audra Wathen <mrsbluerose@gmail.com>
2024-07-18 12:37:02 -0600

    MCL-12: updates user contrl, svc, repo - updates all user classes to operate off of userId in place of username (removes username)

--- src/main/java/com/mycinemalist/repository/UserRepository.java
+++ src/main/java/com/mycinemalist/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.mycinemalist.repository;
 
 import com.mycinemalist.entity.User;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    User findByEmail(String userEmail);
 }
--- src/main/java/com/mycinemalist/service/UserService.java
+++ src/main/java/com/mycinemalist/service/UserService.java
@@ -1,8 +1,11 @@
 package com.mycinemalist.service;
 
 import com.mycinemalist.entity.User;
+import org.bson.types.ObjectId;
+
+import java.util.Optional;
 
 public interface UserService {
     User createUser(User user);
-    User findByUsername(String username);
+    Optional<User> findByUserId(String userId);
 }
