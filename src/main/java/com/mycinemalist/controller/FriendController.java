ea7e44ed935a6a725563c39b07ee158a28aa66f9
Audra Wathen <mrsbluerose@gmail.com>
2024-07-18 21:52:44 -0600

    MCL-12: updates friend controller endpoints - updates add, get and remove endpoints to use UserDTO

--- src/main/java/com/mycinemalist/controller/FriendController.java
+++ src/main/java/com/mycinemalist/controller/FriendController.java
@@ -1,11 +1,13 @@
 package com.mycinemalist.controller;
 
+import com.mycinemalist.DTO.UserDTO;
 import com.mycinemalist.entity.User;
 import com.mycinemalist.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/friends")
@@ -15,17 +17,22 @@ public class FriendController {
     private FriendService friendService;
 
     @PostMapping("/{userId}/add/{friendId}")
-    public User addFriend(@PathVariable String userId, @PathVariable String friendId) {
-        return friendService.addFriend(userId, friendId);
+    public UserDTO addFriend(@PathVariable String userId, @PathVariable String friendId) {
+        Optional<UserDTO> opt = friendService.addFriend(userId, friendId);
+        if(opt.isPresent()) {
+            return opt.get();
+        } else {
+            return null;
+        }
     }
 
     @DeleteMapping("/{userId}/remove/{friendId}")
-    public User removeFriend(@PathVariable String userId, @PathVariable String friendId) {
+    public Boolean removeFriend(@PathVariable String userId, @PathVariable String friendId) {
         return friendService.removeFriend(userId, friendId);
     }
 
     @GetMapping("/{userId}")
-    public List<String> getFriends(@PathVariable String userId) {
+    public List<UserDTO> getFriends(@PathVariable String userId) {
         return friendService.getFriends(userId);
     }
 }
--- src/main/java/com/mycinemalist/service/FriendServiceImpl.java
+++ src/main/java/com/mycinemalist/service/FriendServiceImpl.java
@@ -1,11 +1,15 @@
 package com.mycinemalist.service;
 
+import com.mycinemalist.DTO.UserDTO;
 import com.mycinemalist.entity.User;
 import com.mycinemalist.repository.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FriendServiceImpl implements FriendService {
@@ -14,28 +18,72 @@ public class FriendServiceImpl implements FriendService {
     private UserRepository userRepository;
 
     @Override
-    public User addFriend(String userId, String friendId) {
-        User user = userRepository.findById(userId).orElse(null);
-        if (user != null && !user.getFriends().contains(friendId)) {
-            user.getFriends().add(friendId);
+    public Optional<UserDTO> addFriend(String userId, String friendId) {
+        ObjectId userObjectId = new ObjectId(userId);
+        ObjectId friendObjectId = new ObjectId(friendId);
+
+        Optional<User> optUser = userRepository.findById(userObjectId);
+
+        if (optUser.isPresent() && !optUser.get().getFriends().contains(friendObjectId)) {
+            User user = optUser.get();
+            List<ObjectId> userFriendList = user.getFriends();
+            userFriendList.add(friendObjectId);
+            user.setFriends(userFriendList);
             userRepository.save(user);
+
+            Optional<User> friend = userRepository.findById(friendObjectId);
+            if (friend.isPresent()) {
+                UserDTO friendDTO = UserDTO.builder()
+                        .id(friend.get().getId().toString())
+                        .email(friend.get().getEmail())
+                        .displayName(friend.get().getDisplayName())
+                        .build();
+                return Optional.of(friendDTO);
+            }
         }
-        return user;
+
+        return Optional.empty();
     }
 
     @Override
-    public User removeFriend(String userId, String friendId) {
-        User user = userRepository.findById(userId).orElse(null);
-        if (user != null && user.getFriends().contains(friendId)) {
-            user.getFriends().remove(friendId);
+    public Boolean removeFriend(String userId, String friendId) {
+        ObjectId userObjectId = new ObjectId(userId);
+        ObjectId friendObjectId = new ObjectId(friendId);
+
+        Optional<User> optUser = userRepository.findById(userObjectId);
+        if (optUser.isPresent() && optUser.get().getFriends().contains(friendObjectId)) {
+            User user = optUser.get();
+            List<ObjectId> userFriendList = user.getFriends();
+            userFriendList.remove(friendObjectId);
+            user.setFriends(userFriendList);
             userRepository.save(user);
+            return true;
         }
-        return user;
+        return false;
     }
 
     @Override
-    public List<String> getFriends(String userId) {
-        User user = userRepository.findById(userId).orElse(null);
-        return user != null ? user.getFriends() : null;
+    public List<UserDTO> getFriends(String userId) {
+        List<UserDTO> rtn = new ArrayList<>();
+
+        ObjectId userObjectId = new ObjectId(userId);
+
+        Optional<User> user = userRepository.findById(userObjectId);
+        if(user.isPresent()){
+
+            for(ObjectId friendObjectId : user.get().getFriends()) {
+                Optional<User> friend = userRepository.findById(friendObjectId);
+                if(friend.isPresent()) {
+                    UserDTO userDTO = UserDTO.builder()
+                            .id(friendObjectId.toString())
+                            .email(friend.get().getEmail())
+                            .displayName(friend.get().getDisplayName())
+                            .build();
+                    rtn.add(userDTO);
+                }
+            }
+
+        }
+        return rtn;
     }
 }
